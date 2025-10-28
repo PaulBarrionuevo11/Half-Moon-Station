6debfdf583fdb9dee1ca7bd6a142e1124b1e88ba
PaulBarrionuevo11 <47123187+PaulStein11@users.noreply.github.com>
2025-10-27 21:57:47 -0700

    Advance GPIO class

---
+++ HMB.v.1.1/moon_lab/GPIO.cpp
@@ -0,0 +1,31 @@
+#include "GPIO.h"
+
+float GPIO::read_pin0(uint32_t pinNumber)
+{
+    pt_gpio_in->pin_0 = PIN_STATE_HIGH;
+}
+float GPIO::read_pin1(uint32_t pinNumber)
+{
+    pt_gpio_in->pin_1 = PIN_STATE_HIGH;
+
+}
+float GPIO::read_pin2(uint32_t pinNumber)
+{
+    pt_gpio_in->pin_2 = PIN_STATE_HIGH;
+
+}
+float GPIO::read_pin3(uint32_t pinNumber)
+{
+    pt_gpio_in->pin_0 = PIN_STATE_HIGH;
+
+}
+float GPIO::read_pin4(uint32_t pinNumber)
+{
+    pt_gpio_in->pin_0 = PIN_STATE_HIGH;
+
+}
+float GPIO::read_pin5(uint32_t pinNumber)
+{
+    pt_gpio_in->pin_0 = PIN_STATE_HIGH;
+
+}
--- HMB.v.1.1/moon_lab/GPIO.h
+++ HMB.v.1.1/moon_lab/GPIO.h
@@ -1,35 +1,18 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef GPIO_H_
+#define GPIO_H_
 
 #include<stdint.h>
 
-// Adjust address and matrice
-#define ADD_REG_AHB1ENR		((RCC_AHB1ENR_t*)0x40023830)
-#define ADD_GPIOA_MODE		((GPIOx_MODE_t*)0x40020000)
-#define ADD_GPIOA_OUT		((GPIOx_ODR_t*)0x40020014)
-#define TIME_DELAY			(300000UL)
-#define CLOCK_EN			(1)
-#define MODE_CONF_OUTPUT	(1)
-#define PIN_STATE_HIGH		(1)
-#define PIN_STATE_LOW		(0)
+// ESP32 WROOM GPIO Address Matrix Register
 
-typedef struct
-{
-	uint32_t gpioa_en:1;
-	uint32_t gpiob_en:1;
-	uint32_t gpioc_en:1;
-	uint32_t gpiod_en:1;
+#define GPIO_BASE_ADDR			(0x3FF44000)
+#define GPIO_IN_REG_ADDR		((GPIO_IN_REG*)GPIO_BASE_ADDR + 0x3C);
+#define GPIO_OUT_REG_ADDR		((GPIO_OUT_REG*)GPIO_BASE_ADDR + 0x04);
+#define GPIO_OUT_W1TS_ADDR		((GPIO_OUT_REG*)GPIO_BASE_ADDR + 0x04);
 
-} RCC_AHB1ENR_t;
+#define PIN_STATE_HIGH			(1)
+#define PIN_STATE_LOW			(0)
 
-typedef struct
-{
-	uint32_t pin_0:2;
-	uint32_t pin_1:2;
-	uint32_t pin_2:2;
-	uint32_t pin_3:2;
-
-} GPIOx_MODE_t;
 
 typedef struct
 {
@@ -37,19 +20,48 @@ typedef struct
 	uint32_t pin_1:1;
 	uint32_t pin_2:1;
 	uint32_t pin_3:1;
+	uint32_t pin_4:1;
+	uint32_t pin_5:1;
+	uint32_t pin_6:1;
+	uint32_t pin_7:1;
+	uint32_t pin_8:1;
+	uint32_t pin_9:1;
+	uint32_t pin_10:1;	
+	uint32_t pin_11:1;
+	uint32_t pin_12:1;
+	uint32_t pin_13:1;
+	uint32_t pin_14:1;
+	uint32_t pin_15:1;	
+	uint32_t pin_16:1;
+	uint32_t pin_17:1;
+	uint32_t pin_18:1;
+	uint32_t pin_19:1;
+	uint32_t pin_20:1;
+	uint32_t pin_21:1;
+	uint32_t pin_22:1;
+	uint32_t pin_23:1;
+	uint32_t pin_24:1;
+	uint32_t pin_25:1;	
+	uint32_t pin_26:1;
+	uint32_t pin_27:1;
+	uint32_t pin_28:1;
+	uint32_t pin_29:1;
+	uint32_t pin_30:1;
 
-} GPIOx_ODR_t;
+} GPIO_IO_REG;
 
-class GPIOx_MODE_t
+class GPIO
 
 {
     private:
-        volatile GPIOx_MODE_t* const modeReg;
-        volatile GPIOx_ODR_t* const odrReg;
-        uint32_t pin;
-    public:
+        volatile GPIO_IO_REG* const pt_gpio_IO;
+		uint32_t pin;
+		
 
+    public:	
+		float read_pin(uint32_t pin);
+		void write_pin(uint32_t pin);
 
 }
 
-#endif /* MAIN_H_ */
+#endif 
